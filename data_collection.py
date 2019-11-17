506eae1fb2e4e026aeeaeae93f2e7dda929045f3
dalouis <louis.dannatt@mail.mcgill.ca>
2019-11-17 15:52:45 -0500

    fixed dataframe layout and centralized the count

--- data_collection.py
+++ data_collection.py
@@ -7,20 +7,24 @@ import pickle
 # Dict assigning an integer label to each of the topics we have 
 #label_dict={topic:i for topic,i in enumerate(topics)}
 
-def getQuery(queryList,topic,data):
+def getDivider(numTopic, numKeyword): 
+    return numTopic*numKeyword
+def getQuery(queryList,topic,data,requiredNum,curNum):
     # set up how much data to retrieve !make sure that all lengths add up for dataframe
-    count = 40 # Nbr of tweets to get for each keyword
-    if topic is 'general':
-        count = 200
+    divider = getDivider(5,len(queryList))
+    count = requiredNum/divider
+    if topic is '4':
+        count = requiredNum - curNum
     # access api (note I had to used mixed for result type instead of popular to get the desired amount of tweets)
     for keyword in queryList: 
+        curNum = curNum + count
         query = {'q': keyword,
          'result_type': 'mixed',
          'count': count,
-         'lang': 'en'}
+         'lang': 'en'} 
         for status in python_tweets.search(**query)['statuses']:
-            data[topic].append(status['text'])
-    return data 
+            data.append([status['text'],int(topic)])
+    return data, curNum
 
 
 # create object with twitter keys
@@ -34,31 +38,32 @@ queryCompanies = ['apple','microsoft','tesla','amazon','google']
 # use most common character in english to get general tweets on twitter
 queryGeneral =['e']
 
-# make dict of queries
-queryDict = {'politics': queryPolitics, 'sports': querySports, 'movies': queryMovies, 'companies': queryCompanies, 'general': queryGeneral}
+# make dict of queries [politics = 0, sports = 1, movies = 2, companies = 3, general = 4]
+queryDict = {'0': queryPolitics, '1': querySports, '2': queryMovies, '3': queryCompanies, '4': queryGeneral}
 # make dict that contains each topic
-data = {'politics': [], 'sports': [], 'movies': [], 'companies': [], 'general': []}
-
+data = []
+requiredNum = 200000
+curNum = 0
 # iterate through queries and retrieve data from twitter
 for key in queryDict: 
-    data = getQuery(queryDict[key],key,data)
+    data,curNum= getQuery(queryDict[key],key,data,requiredNum,curNum)
     
-for k,v in data.items():
-    print(len(v))
+# for k,v in data.items():
+#     print(len(v))
 
 # make data pandas dataframe to facilitate manipulation
-df = pd.DataFrame(data)
+df = pd.DataFrame(data, columns = ['Tweet','Label'])
 
 # save the data for further processing 
 # Save in 'data' directory 
 
-toydata={'tweet':df['general'],'label':np.ones(100)}
+# toydata={'tweet':df['4'],'label':np.ones(100)}
 
-df = pd.DataFrame.from_dict(toydata)
+# df = pd.DataFrame.from_dict(toydata)
 
-#df.to_csv('example_dataframe.csv')
+df.to_csv('dataframe.csv')
 
 """
 pickle.dump(df, open('data/rawTwitterData.pickle', 'wb'))
-print(df['politics'][6])
+print(df['0'][6])
 """
