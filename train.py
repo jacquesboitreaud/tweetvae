86584e84d0e5e1a4030f5790a80cf36eef0f4b62
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2019-11-21 19:46:37 -0500

    testing

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
     SAVE_MODEL=True
     
     #Load train set and test set
-    loaders = Loader(num_workers=4, batch_size=batch_size, clean= True, max_n=200000)
+    loaders = Loader(num_workers=4, batch_size=batch_size, clean= True, max_n=4000)
     train_loader, _, test_loader = loaders.get_data()
     # Save vocabulary for later (evaluation):
     pickle.dump(loaders.dataset.words_to_ids,open("./saved_model_w/vocabulary.pickle","wb"))
